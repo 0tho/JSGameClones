c1a16b2c01d01b09773927f96c768117be37f757
Cristiano Faustino <cristiadu@gmail.com>
2015-07-30 11:17:16 -0300

    Including functions needed

--- Snake/javascript/cristiano/Snake.js
+++ Snake/javascript/cristiano/Snake.js
@@ -1,15 +1,92 @@
 (function () {
 
-    function Snake () {
-
+    function SnakeGame () {
+    	this.snake = new SnakeHead(),
+    	this.food = new Food()
     }
 
-    Snake.prototype.update = function () {
+ 	function SnakeBody () {
+		this.next = null;
+    	this.direction = "left";
+    	this.X = ;
+    	this.Y = ;
+    	this.update = function()
+    	{
+    		if(this.next != null)
+    			next.update();
+    	};
+    	this.draw = function()
+    	{
+    		if(this.next != null)
+    			next.draw();
+    	};
+    	this.increase = function()
+    	{
+    		if(next != null)
+    			next.increase();
+    		else
+    		{
+    			next = new SnakeBody();
+    		}
+    	};
+    }
 
+    function SnakeHead () {
+    	this.body = new SnakeBody();
+    	this.direction = "left";
+    	this.X = ;
+    	this.Y = ;
+    	this.update = function()
+    	{
+    		body.update();
+    	};
+    	this.draw = function()
+    	{
+    		body.draw();
+    	};
+    	this.init = function()
+    	{
+    		body.increase();
+    	};
+
+    	this.checkCollision = function(){};
+    	this.changeDirection = function(){};
+    	this.isEatingFood = function(){ return false;};
+    }
+
+    function Food () {
+    	this.X = ;
+    	this.Y = ;
+    	this.elapsedTime = ;
+    	this.update = function(){};
+    	this.draw = function(){};
+    	this.generateFood = function(){};
+    	this.init = function(){};
+    }
+
+    SnakeGame.prototype.update = function () {
+    	food.update();
+    	snake.update();
     };
 
-    Snake.prototype.draw = function () {
+    SnakeGame.prototype.draw = function () {
+    	food.draw();
+		snake.draw();
     };
 
-    window.Snake = Snake;
+  	SnakeGame.prototype.checkInput = function () {
+    	// Process inputs
+
+    	snake.changeDirection(direction);
+    };
+
+    SnakeGame.prototype.init = function () {
+    	snake.init();
+    	food.init();
+    };
+
+    SnakeGame.prototype.gameOver = function () {
+    };
+
+    window.SnakeGame = SnakeGame;
 })();
