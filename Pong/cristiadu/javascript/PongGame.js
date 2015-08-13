9cea4cc00187397b25ca4cbc580af1a611d12915
Cristiano Faustino <cristiadu@gmail.com>
2015-08-13 10:37:35 -0300

    Game fully working, some problems might appear

--- Pong/cristiadu/javascript/PongGame.js
+++ Pong/cristiadu/javascript/PongGame.js
@@ -8,9 +8,13 @@ function PongGame (w,h) {
 
 	
 PongGame.prototype.update = function () {
+
+	this.ball.update();
+	
+	
+
 	this.player1Paddle.update();
 	this.player2Paddle.update();
-	this.ball.update();
 
 	this.checkCollisionBallwithPaddle();
 };
@@ -19,9 +23,14 @@ PongGame.prototype.draw = function (ctx,dt) {
   	ctx.fillStyle = '#000';
     ctx.fillRect( 0, 0, this.width, this.height);
 
+	this.ball.draw(ctx,dt);
 	this.player1Paddle.draw(ctx,dt);
 	this.player2Paddle.draw(ctx,dt);
-	this.ball.draw(ctx,dt);
+	
+
+	ctx.fillStyle = '#fff';
+	ctx.font = "48px Monospace";
+  	ctx.fillText(this.player1Paddle.points+" X "+this.player2Paddle.points, (game.width-100)/2, 50);
 };
 
 PongGame.prototype.checkCollisionBallwithPaddle = function () 
@@ -33,7 +42,7 @@ PongGame.prototype.checkCollisionBallwithPaddle = function ()
 	{
 		var bounceAngle = this.player1Paddle.getBounceAngle(this.ball.Y);
 		this.ball.changeDirection(bounceAngle);
-		
+				
 	}
 	else if(this.ball.X > this.player2Paddle.X && this.ball.X < this.player2Paddle.X+THICKNESS_PADDLE && this.ball.Y > this.player2Paddle.Y && this.ball.Y < this.player2Paddle.Y+SIZE_PADDLE) 
 	{
@@ -41,11 +50,18 @@ PongGame.prototype.checkCollisionBallwithPaddle = function ()
 		this.ball.changeDirection(bounceAngle);
 	}
 
+
+	
 };
 
 PongGame.prototype.scorePoint = function (pos) 
 {
-	//this.ball.init();
+	if(pos == POSITION.LEFT)
+		this.player2Paddle.points++;
+	else if(pos == POSITION.RIGHT)
+		this.player1Paddle.points++;
+
+	this.ball.init();
 };
 
 PongGame.prototype.init = function () {
